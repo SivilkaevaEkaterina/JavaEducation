8dfeb3f7454024a0c7a64a73d3815b38e30778ad
sivilkaevaekaterina <devafairys@gmail.com>
2022-11-26 22:05:34 +0500

    update 26/11/2022 ver2

--- src/lr1/Example7.java
+++ src/lr1/Example7.java
@@ -6,13 +6,13 @@ public class Example7 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Input: ");
+        System.out.println("Input name: ");
         String name = in.nextLine();
 
         System.out.println("Input age: ");
         int age = in.nextInt();
 
-        System.out.println("Name and Age: " + name + " " + age);
+        System.out.println("Name and Age: " + name + ", " + age);
 
     }
 }
