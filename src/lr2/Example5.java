b1f69c7967853568f1ce9900434c8d3a9517e8d1
sivilkaevaekaterina <devafairys@gmail.com>
2022-12-01 22:56:25 +0500

    update 26/11/2022 ver2

---
+++ src/lr2/Example5.java
@@ -0,0 +1,20 @@
+package lr2;
+
+import java.util.Scanner;
+
+public class Example5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input a number: ");
+        int number = in.nextInt();
+
+        int result = number / 1000 % 10;
+
+        System.out.println(result);
+
+
+
+
+    }
+}
