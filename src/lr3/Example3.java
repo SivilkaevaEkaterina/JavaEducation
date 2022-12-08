4386f29c5981ae8f421502bd37a16ed5fbd8e513
sivilkaevaekaterina <devafairys@gmail.com>
2022-12-08 23:10:17 +0500

    update 26/11/2022 ver2

--- src/lr3/Example3.java
+++ src/lr3/Example3.java
@@ -1,42 +1,32 @@
 package lr3;
 
 import java.util.Scanner;
-import java.util.Arrays;
-
 public class Example3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите размер массива");
-        int number = in.nextInt();
 
-        System.out.println("Размер массива равен"+number);
-        int[] randomNumbers = new int[number];
-        for (int i=0; i<randomNumbers.length; i++ ) {
+        System.out.println("Введите размер массива ");
+        int num = in.nextInt();
+        int[] numbers = new int[num];
+        numbers[0] = 1;
+        numbers[1] = 1;
+        for (int i = 2; i<numbers.length; i++) {
+            numbers[i] = numbers[i - 1] + numbers[i - 2];
 
-            Arrays.sort(randomNumbers);
-            System.out.println("Элемент массива["+i+"]=" + randomNumbers[i]);
+        }
+        for (int i = 0; i<numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
         }
 
 
-
-
-        }
     }
 
 
 
 
-
-
-
-
-
-
-
-
-   // Напишите программу, которая выводит последовательность чисел
-    //    Фибоначчи. Первые два числа в этой последовательности равны 1, а каждое
-    //    следующее число равно сумме двух предыдущих (получается последовательность 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 и так далее). Количество чисел
-     //   в последовательности вводится пользователем. Предложите версии
-     //   программы, использующие разные операторы цикла.
-
+// Напишите программу, которая выводит последовательность чисел
+//    Фибоначчи. Первые два числа в этой последовательности равны 1, а каждое
+//    следующее число равно сумме двух предыдущих (получается последовательность 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 и так далее). Количество чисел
+//   в последовательности вводится пользователем. Предложите версии
+//   программы, использующие разные операторы цикла.
