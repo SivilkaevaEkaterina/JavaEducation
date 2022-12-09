a04d3ab5b13ac68c98fc174b79b9d39c8d1b5d36
sivilkaevaekaterina <devafairys@gmail.com>
2022-12-09 17:17:50 +0500

    update 26/11/2022 ver2

--- src/lr3/Example4a.java
+++ src/lr3/Example4a.java
@@ -1,21 +1,23 @@
 package lr3;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
-public class Example4 {
+public class Example4a {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         System.out.println("Введите первое число");
         int first = in.nextInt();
 
         System.out.println("Введите второе число");
         int second = in.nextInt();
 
-        if (first>second){
-            System.out.println(second + "," + first);
-        } else {
-            System.out.println(first + "," + second);
+        int[] result = {first, second};
+
+        Arrays.sort(result);
+
+        for (int i = 0; i<2; i++) {
+            System.out.println(result[i]);
         }
 
 
