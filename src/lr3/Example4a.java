5b4f7647c116d341e1d7c8f21fc6302bc0a877da
sivilkaevaekaterina <devafairys@gmail.com>
2022-12-09 17:26:37 +0500

    update 26/11/2022 ver2

--- src/lr3/Example4a.java
+++ src/lr3/Example4a.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 public class Example4a {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите первое число");
+        System.out.println(" Введите первое число " );
         int first = in.nextInt();
 
-        System.out.println("Введите второе число");
+        System.out.println(" Введите второе число " );
         int second = in.nextInt();
 
         int[] result = {first, second};
