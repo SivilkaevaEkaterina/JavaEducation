6cb8efbf5e8321024f48877ed4e4872f58cab8a5
sivilkaevaekaterina <devafairys@gmail.com>
2022-12-11 11:56:33 +0500

    update 26/11/2022 ver2

--- src/lr3/Example6.java
+++ src/lr3/Example6.java
@@ -4,27 +4,31 @@ import java.util.Scanner;
 import java.util.Random;
 public class Example6 {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
 
         System.out.println("Введите размер массива ");
         int size = in.nextInt();
 
-        int[] numbers = new int[size];
-        Random rand = new Random();
-        int i = 0;
-        while (i != size) {
+        if (0 < size) {
 
+            int[] numbers = new int[size];
+            Random rand = new Random();
+            int i = 0;
 
-            int result = rand.nextInt(100);
-            if (result % 5 == 2) {
-                numbers [i] = result ;
-                i++;
+            while (i != size) {
+
+                int result = rand.nextInt(50);
+                if (result % 5 == 2) {
+                    numbers[i] = result;
+                    i++;
+                }
             }
-
-
+            for (int numb : numbers) {
+                System.out.println(numb);
+            }
+        } else {
+            System.out.println("Размер массива не может быть меньше или равен нулю");
         }
-        for (int numb:numbers) {
-            System.out.println(numb);
-        }
-        }
+    }
 }
