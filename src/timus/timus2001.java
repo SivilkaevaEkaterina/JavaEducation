8feec80dca02c4c23cf7144da8d5ab63535a4e6a
sivilkaevaekaterina <devafairys@gmail.com>
2022-12-11 20:47:36 +0500

    update 26/11/2022 ver2

---
+++ src/timus/timus2001.java
@@ -0,0 +1,22 @@
+package timus;
+
+import java.util.Scanner;
+
+public class timus2001 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a1 = in.nextInt();
+        int b1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b2 = in.nextInt();
+        int a3 = in.nextInt();
+        int b3 = in.nextInt();
+
+        int result = b1-b2;
+        int result2 = b3-result-b2;
+
+        System.out.println(result2 + " " + result);
+
+    }
+}
