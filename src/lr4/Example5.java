84fe116e3dcae5590b60f452e395bbdabd78ed30
sivilkaevaekaterina <devafairys@gmail.com>
2022-12-18 21:32:55 +0500

    update 26/11/2022 ver2

--- src/lr4/Example5.java
+++ src/lr4/Example5.java
@@ -5,33 +5,21 @@ import java.util.Random;
 public class Example5 {
     public static void main(String[] args) {
         Random random = new Random();
-        int figure[][] = new int[3][3];
-        for (int i = 0; i < figure.length; i++) {
-            for (int j = 0; j < figure[i].length; j++) {
+        int a = 3;
+        int b = 5;
+        int figure[][] = new int[a][b];
+        int second[][] = new int[b][a];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
                 figure[i][j] = random.nextInt(9);
-                System.out.print(figure[i][j]+" ");
-
+                System.out.print(figure[i][j] + " ");
             }
             System.out.println();
         }
-        reverse(figure);
-        printArray(figure);
-    }
-    public static void reverse(int changing[][]) {
-        System.out.println(" ");
-        for(int i=0; i< changing.length-1; i++) {
-            for(int j=i+1; j< changing[i].length; j++) {
-                int temp = changing[i][j];
-                changing[i][j] = changing[j][i];
-                changing[j][i] = temp;
-
-            }
-        }
-    }
-    public static void printArray(int anotherFigure[][]){
-        for(int i=0;i<anotherFigure.length; i++) {
-            for(int j = 0; j< anotherFigure[i].length; j++) {
-                System.out.print(anotherFigure[i][j]+" ");
+        for (int p=0; p<b;p++){
+            for (int k=0;k < a;k++){
+                second [p][k]= figure [k][p];
+                System.out.print(second[p][k]);
             }
             System.out.println();
         }
