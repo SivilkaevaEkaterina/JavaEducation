81e0c457fb566fe11d0496b993b1b86f88cfd76f
sivilkaevaekaterina <devafairys@gmail.com>
2022-12-19 00:49:25 +0500

    update 26/11/2022 ver2

--- src/lr4/Example6.java
+++ src/lr4/Example6.java
@@ -7,7 +7,7 @@ public class Example6 {
         int[][] first = {{1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}};
-        int[][] second = new int[3][3];
+        int[][] second = new int[2][2];
         int a = random.nextInt(3);
         int b = random.nextInt(3);
 
@@ -17,16 +17,22 @@ public class Example6 {
             }
             System.out.println();
         }
+        int x=0;
+        int y=0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (i != a && j != b) {
-                    second[i][j] = first[i][j];
+                    second[x][y] = first[i][j];
+                    y=(y+1) %2;
+                    if (y==0 || j==2) {
+                        x++;
+                    }
                 }
             }
         }
         System.out.println("строка " + a + " столбец " + b);
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
                 System.out.print(second[i][j]);
             }
             System.out.println();
